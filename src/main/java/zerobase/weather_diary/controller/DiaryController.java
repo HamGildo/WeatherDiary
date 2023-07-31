c56a4c435374f6e397b049c744693a714a9bc3d5
HamGildo <ham5477@naver.com>
2023-08-01 00:06:32 +0900

    feat: Add read-diary-by-date api

--- src/main/java/zerobase/weather_diary/controller/DiaryController.java
+++ src/main/java/zerobase/weather_diary/controller/DiaryController.java
@@ -1,13 +1,12 @@
 package zerobase.weather_diary.controller;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import zerobase.weather_diary.domain.Diary;
 import zerobase.weather_diary.service.DiaryService;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController // 상태코드 알려줌
 public class DiaryController {
@@ -21,5 +20,10 @@ public class DiaryController {
     @PostMapping("/create/diary")
     void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.createDiary(date, text);
+    }
+
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        return diaryService.readDiary(date);
     }
 }
