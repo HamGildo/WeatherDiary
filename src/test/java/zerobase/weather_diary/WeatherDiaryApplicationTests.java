8fddc60f22604801bfd156a3b839536ea28e1678
HamGildo <ham5477@naver.com>
2023-07-31 20:25:39 +0900

    init project

---
+++ src/test/java/zerobase/weather_diary/WeatherDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.weather_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
